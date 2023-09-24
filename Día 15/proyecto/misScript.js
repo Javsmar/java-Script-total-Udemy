4e04a6dd2b69b08c802f145de10caf0f50b1bedc
Javier <mastos44@gmail.com>
2023-09-24 17:54:14 +0200

    cambio final

--- Día 15/proyecto/misScript.js
+++ Día 15/proyecto/misScript.js
@@ -1,23 +1,16 @@
 async function obtenerTodos(){
-    try {
-        // Realiza una solicitud GET para obtener todos los dispositivos
-        const response = await fetch('https://my-json-server.typicode.com/fedegaray/telefonos/db', {
+    try{
+        fetch('https://my-json-server.typicode.com/fedegaray/telefonos/db',{
             method: 'GET',
             headers: {
                 "Content-Type": "application/json"
             }
-        });
-
-        if (response.ok) {
-            // Convierte la respuesta en formato JSON
-            const data = await response.json();
-
-            // Obtiene el elemento de la tabla donde se mostrarán los dispositivos
+        }) 
+        .then(res => res.json())
+        .then(data => {
             let cuerpoTabla = document.getElementById('tblContenido');
             let exit = "";
-
-            // Itera a través de los dispositivos y crea filas para la tabla
-            for (let element of data.dispositivos) {
+            for(let element of data.dispositivos){
                 exit += `
                 <tr>
                     <td>${element.id}</td>
@@ -29,62 +22,45 @@ async function obtenerTodos(){
                 </tr>
                 `;
             }
-            
-            // Inserta las filas en la tabla
             cuerpoTabla.innerHTML = exit;
-        } else {
-            throw new Error('Error en la solicitud: ' + response.statusText);
-        }
-    } catch (error) {
-        console.error(error);
+        })
+        .catch(error => {throw new Error("Error en la solicitud" + error)})
+    } catch(error){
+        console.error(error)
     }
 }
 
 async function consultarUno(){
-    try {
+    try{
         let id = document.getElementById('txtConsulta').value;
-        if (id === '') {
+        if(id === ''){
             alert('No se ha ingresado ningún ID');
             return;
         }
-
-        // Realiza una solicitud GET para consultar un dispositivo por su ID
-        const response = await fetch(`https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/${id}`, {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (response.ok) {
-            // Convierte la respuesta en formato JSON
-            const dispositivo = await response.json();
-
-            // Llena los campos del formulario con los datos del dispositivo
+        axios.get('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/' + id )
+        .then(res => {
+            let dispositivo = res.data;
             document.getElementById('consultarNombre').value = dispositivo.marca;
             document.getElementById('consultarModelo').value = dispositivo.modelo;
             document.getElementById('consultarColor').value = dispositivo.color;
             document.getElementById('consultarAlmacenamiento').value = dispositivo.almacenamiento;
             document.getElementById('consultarProcesador').value = dispositivo.procesador;
-        } else {
-            throw new Error('Error en la solicitud: ' + response.statusText);
-        }
-    } catch (error) {
-        console.error(error);
+        })
+        .catch(error => { throw new Error("Error en la solicitud: " + error.message)})
+    } catch(error){
+        console.error(error)
     }
 }
 
 async function agregarUno(){
-    try {
-        // Obtiene los valores del nuevo dispositivo desde los campos del formulario
+    try{
         let marca = document.getElementById('inputmarca').value;
         let modelo = document.getElementById('inputModelo').value;
         let color = document.getElementById('inputColor').value;
         let almacenamiento = document.getElementById('inputAlmacenamiento').value;
         let procesador = document.getElementById('inputProcesador').value;
 
-        // Realiza una solicitud POST para agregar un nuevo dispositivo
-        const response = await fetch('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/', {
+        fetch('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/',{
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
@@ -96,44 +72,37 @@ async function agregarUno(){
                 almacenamiento: almacenamiento,
                 procesador: procesador
             })
-        });
-
-        if (response.ok) {
-            // Actualiza la tabla de dispositivos
+        })
+        .then(res => res.json())
+        .then(data => {
             obtenerTodos();
-
-            // Muestra una alerta con los datos del nuevo dispositivo
-            const data = await response.json();
             alert(`Se ha agregado un nuevo dispositivo:
             \nMarca: ${data.marca}
             \nModelo: ${data.modelo}
             \nColor: ${data.color}
             \nAlmacenamiento: ${data.almacenamiento}
-            \nProcesador: ${data.procesador}`);
-        } else {
-            throw new Error('Error en la solicitud: ' + response.statusText);
-        }
-    } catch (error) {
+            \nProcesador: ${data.procesador}`)
+        })
+        .catch(error => {throw new Error("Error en la solicitud" + error)})
+    }catch(error){
         console.error(error);
     }
 }
 
 async function modificarUno(){
-    try {
-        let id = document.getElementById('txtConsulta').value;
+    try{
+        let id = document.getElementById('txtConsulta').value; 
         let nombre = document.getElementById('consultarNombre').value;
         let modelo = document.getElementById('consultarModelo').value;
         let color = document.getElementById('consultarColor').value;
         let almacenamiento = document.getElementById('consultarAlmacenamiento').value;
         let procesador = document.getElementById('consultarProcesador').value;
 
-        if (nombre === '') {
+        if(nombre === ''){
             alert('El registro no está completo');
             return;
         }
-
-        // Realiza una solicitud PUT para modificar un dispositivo existente
-        const response = await fetch(`https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/${id}`, {
+        fetch('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/' + id,{
             method: 'PUT',
             headers: {
                 "Content-Type": "application/json"
@@ -146,50 +115,39 @@ async function modificarUno(){
                     almacenamiento: almacenamiento,
                     procesador: procesador
                 }
+                
             })
-        });
-
-        if (response.ok) {
-            // Muestra una alerta con los datos del dispositivo modificado
-            const data = await response.json();
+        })
+        .then(res => res.json())
+        .then(data => {
             alert(`Se ha modificado el archivo ${id}. Nuevo contenido:\n ${JSON.stringify(data)}`);
-        } else {
-            throw new Error('Error en la solicitud: ' + response.statusText);
-        }
-    } catch (error) {
+        })
+        .catch(error => {throw new Error("Error en la solicitud" + error)})
+    }catch(error){
         console.error(error);
     }
 }
 
 async function EliminarUno(){
-    try {
+    try{
         let id = document.getElementById('txtConsulta').value;
-        if (id === '') {
+        if(id === ''){
             alert('No ha ingresado ningún ID');
             return;
         }
-
-        // Realiza una solicitud DELETE para eliminar un dispositivo por su ID
-        const response = await fetch(`https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/${id}`, {
-            method: 'DELETE'
-        });
-
-        if (response.ok) {
-            // Limpia los campos del formulario
+        axios.delete('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/' + id)
+        .then(res => {
+            alert(`Se ha eleiminado el archivo ${id}.`);
             document.getElementById('consultarNombre').value = "";
             document.getElementById('consultarModelo').value = "";
             document.getElementById('consultarColor').value = "";
             document.getElementById('consultarAlmacenamiento').value = "";
             document.getElementById('consultarProcesador').value = "";
 
-            // Actualiza la tabla de dispositivos
             obtenerTodos();
-
-            alert(`Se ha eliminado el archivo ${id}.`);
-        } else {
-            throw new Error('Error en la solicitud: ' + response.statusText);
-        }
-    } catch (error) {
+        })
+        .catch(error => {throw new Error("Error en la solicitud" + error)})
+    }catch(error){
         console.error(error);
     }
 }
